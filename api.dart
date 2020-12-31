62a05cea8dc3aa591739296a8dceafac453a524a
VeniKhariyunnisa <74498264+VeniKhariyunnisa@users.noreply.github.com>
2020-12-31 14:55:32 +0700

    Add files via upload

---
+++ api.dart
@@ -0,0 +1,12 @@
+
+import 'package:inputsoal_app/url/server.dart';
+
+class UrlAPI{
+  static const String tambahsoal          = ServerLokal.HOSTING +"tambahsoal.php";
+  static const String tambahsoalessay      = ServerLokal.HOSTING +"tambahsoal_e.php";
+  static const String tampilsoalP          = ServerLokal.HOSTING +"daftarsoalP.php";
+  static const String tampilsoalE          = ServerLokal.HOSTING +"daftarsoalE.php";
+  static const String inputjawabanE          = ServerLokal.HOSTING +"inputjawabanE.php";
+  static const String tampilplj          = ServerLokal.HOSTING +"tampilplj.php";
+  static const String tambahplj          = ServerLokal.HOSTING +"tambahplj.php";
+}
